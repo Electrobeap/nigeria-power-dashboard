3b8e2ea6a812099d8532d2a1df3c791bbcebc18f
Paul Atakere <48590119+Electrobeap@users.noreply.github.com>
2026-04-14 20:29:37 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from scraper import fetch_grid_data
+from backend.scraper import fetch_grid_data
 
 app = FastAPI()
 
@@ -16,6 +16,11 @@ app.add_middleware(
 	CORSMiddleware,
 	allow_origins=["*"], 
 	allow_credentials=False,	
-	allow_methods=["*"],													allow_headers=["*"],
+	allow_methods=["*"],													
+	allow_headers=["*"],
 )	
-	
+
+import uvicorn 
+
+if __name__ == "__main__":
+	uvicorn.run("backend.main:app", host="0.0.0.0", port=10000)
